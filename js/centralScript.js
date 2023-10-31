7d3d94ce6c58c15c9dee7c001a3cd65f41489d98
joaoroquesh <84862648+joaoroquesh@users.noreply.github.com>
2023-10-31 12:07:07 -0300

    cores

--- js/centralScript.js
+++ js/centralScript.js
@@ -1,18 +1,19 @@
 function loadScripts() {
     var scripts = {
-        jquery: "https://code.jquery.com/jquery-3.7.1.min.js",
-        bootstrap: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js../script/bootstrap.bundle.min.js"
+        jquery: {url: "https://code.jquery.com/jquery-3.7.1.min.js", load: true},
+        popper: {url: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js", load: true},
+        bootstrap: {url: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js", load: true}
     };
-    
 
     for (var key in scripts) {
-        // Se a variável correspondente for true, carrega o script
-        if (window[key]) {
+        // Se a variável correspondente for true ou não estiver definida, carrega o script
+        if (window[key] !== false && scripts[key].load) {
             var script = document.createElement("script");
-            script.src = scripts[key];
+            script.src = scripts[key].url;
             document.body.appendChild(script);
         }
     }
 }
 
 loadScripts();
+
